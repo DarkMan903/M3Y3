72925474175c9e22c5aef848d5216c5d02e673f8
DarkMan903 <156571062+DarkMan903@users.noreply.github.com>
2024-03-31 17:10:16 +1000

    main.py

--- M3Y2/main.py
+++ M3Y2/main.py
@@ -56,7 +56,7 @@ def submit_form():
     date = request.form['date']
 
     with open('form.txt', 'a',) as f:
-            f.write(name + email + address + date)
+            f.write(name + email + address + date + '\n')
 
     # здесь вы можете сохранить данные или отправить их по электронной почте
     return render_template('form_result.html', 
